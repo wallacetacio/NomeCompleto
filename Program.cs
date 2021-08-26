07d6273bd1a99278ccfc52604451f4cbd6aeb60d
Wallace Tácio <wallacett.dev@gmail.com>
2021-08-26 00:13:23 -0300

    Adiciona resolução do exercício Nome Completo ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string primeiroNome, sobrenome;
+            
+            Console.Write("Por gentileza, digite seu primeiro nome: ");
+            primeiroNome = Console.ReadLine();
+
+            Console.Write("E agora, por favor, digite seu sobrenome: ");
+            sobrenome = Console.ReadLine();
+
+            Console.WriteLine($"Nome completo: {primeiroNome} {sobrenome}");
+            Console.WriteLine($"Nome catálogo: {sobrenome.ToUpper()}, {primeiroNome}");
+        }
+    }
+}
